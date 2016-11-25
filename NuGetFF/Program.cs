8c42c9dbf0fa78a1238afbdb857d2ee580d2030c
Anjou <krzysztof.jan.kroczak@gmail.com>
2016-11-25 21:41:08 +0100

    Nuget start

---
+++ NuGetFF/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetFF
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var host = new ServiceStack.Host.ActionContext();
+		}
+	}
+}
